effed11ab698da8a32dca8b9e3f6850628d6b563
Thiago Silva <titusilvs91@gmail.com>
2023-07-04 00:07:52 -0300

    Refatorando Modal

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,27 +1,51 @@
-export default function initModal() {
-  const btnAbrir = document.querySelector("[data-modal='abrir']");
-  const btnFechar = document.querySelector("[data-modal='fechar']");
-  const containerModal = document.querySelector("[data-modal='container']");
+export default class Modal{
+  constructor(btnAbrir, btnFechar, containerModal){
+  this.btnAbrir = document.querySelector(btnAbrir);
+  this.btnFechar = document.querySelector(btnFechar);
+  this.containerModal = document.querySelector(containerModal);
 
-  if (btnAbrir && btnFechar && containerModal) {
-    function abrirModal(event) {
-      event.preventDefault();
-      containerModal.classList.add("ativo");
+  //bind this ao callback para
+  //fazer referência ao objeto
+  //da class
+  
+  this.eventToggleModal = this.eventToggleModal.bind(this);
+  this.clickForaModal = this.clickForaModal.bind(this);
+
+  };
+ 
+  //Utiliza o toggle para usar classe "ativo"
+  toggleModal() {
+    this.containerModal.classList.toggle("ativo");
+  }; 
+
+  //Previne o evento padrão, e ativa meu metodo de toggle
+  eventToggleModal(event){
+    event.preventDefault();
+    this.toggleModal();
+  };
+
+  //Fechar o modal ao clicar fora
+  clickForaModal(event) {
+    if (event.target === this.containerModal) {
+      this.toggleModal(event);
     }
+  }; 
 
-    function fecharModal(event) {
-      event.preventDefault();
-      containerModal.classList.remove("ativo");
-    }
+  //Adicona os eventos ao modal e seus respectivos elememntos
+  addEventModal(){
+    this.btnAbrir.addEventListener("click",  this.eventToggleModal);
+    this.btnFechar.addEventListener("click",  this.eventToggleModal);
+    this.containerModal.addEventListener("click",  this.clickForaModal);
+  };
 
-    function clickForaModal(event) {
-      if (event.target === containerModal) {
-        fecharModal(event);
-      }
-    }
+  //Inicia meus metodos de eventos no modal, caso exite os 
+  //paramêtros definidos do meu constructor
+  init(){
+    if (this.btnAbrir && this.btnFechar && this.containerModal){
+      this.addEventModal();
+    }  
+    return this;
+  };
+};
 
-    btnAbrir.addEventListener("click", abrirModal);
-    btnFechar.addEventListener("click", fecharModal);
-    containerModal.addEventListener("click", clickForaModal);
-  }
-}
+
